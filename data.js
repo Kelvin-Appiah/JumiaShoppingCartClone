c352912523d69589431b8c13b2af15ce9d9ba20c
Kelvin-Squishy <obengappiah98@gmail.com>
2022-09-22 10:18:05 +0800

    improve UI

---
+++ data.js
@@ -0,0 +1,58 @@
+const products = [
+  {
+    name: 'Sport Elastic Breathable Pants - Grey',
+    seller: 'Kili Brand Shop-SEA-COD | ',
+    size: 'M',
+    image: '/images/channel-1.jpeg',
+    price: 'GH$ 30.10',
+    preprice: 'GH$ 41.00',
+    discount: '-27%',
+    id: 1,
+  },
+
+  {
+    name: 'Kellogg Corn Flakes - 300g',
+    seller: 'Jumia | ',
+    size: 'NA',
+    image: '/images/channel-2.jpeg',
+    price: 'GH$ 12.40',
+    preprice: 'GH$ 19.29',
+    discount: '-36%',
+    id: 2,
+  },
+
+  {
+    name: 'Long Sleeve Shirt - Black',
+    seller: 'Wakanda Stores | ',
+    size: 'XL',
+    image: '/images/channel-3.jpeg',
+    price: 'GH$ 80.00',
+    preprice: 'GH$ 100.00',
+    discount: '-20%',
+    id: 3,
+  },
+
+  {
+    name: 'Baseball Helmet - Blue',
+    seller: 'Kili Brand Shop-SEA-COD | ',
+    size: 'L',
+    image: '/images/channel-5.jpeg',
+    price: 'GH$ 200.10',
+    preprice: 'GH$ 350.100',
+    discount: '-15%',
+    id: 6,
+  },
+
+  {
+    name: 'TomFord Jacket - White',
+    seller: 'Squishy Boutique | ',
+    size: 'S',
+    image: '/images/channel-6.jpeg',
+    price: 'GH$ 400.10',
+    preprice: 'GH$ 500.00',
+    discount: '-50%',
+    id: 5,
+  },
+];
+
+export default products;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,111 @@
 import styles from '../styles/Home.module.css';
 import { useState } from 'react';
+import Image from 'next/image';
+
+import Products from '../data';
 
 export default function Home() {
-  return <h1>Hey</h1>;
+  let cartItemNum = Products.length;
+
+  const [numItems, setNumItems] = useState(`(${cartItemNum})`);
+
+  const [products, setProducts] = useState(Products);
+
+  const [quantity, setQuantity] = useState(1);
+
+  const delProduct = (id) => {
+    setProducts(products.filter((product) => product.id !== id));
+  };
+
+  return (
+    <section className={styles.section}>
+      <h1
+        style={{
+          margin: '0 1rem',
+          borderBottom: '2px solid #000',
+        }}
+      >
+        Cart {numItems}
+      </h1>
+      <hr />
+
+      {products.map((product) => {
+        const { name, image, id, seller, size, price, preprice, discount } =
+          product;
+
+        return (
+          <div key={id}>
+            <div className={styles.itemBox}>
+              <div className={styles.image}>
+                <Image src={image} width={150} height={180} alt={name} />
+              </div>
+              <div>
+                <h2>{name}</h2>
+                <p>
+                  <span style={{ color: 'gray' }}>Seller: </span> {seller}{' '}
+                  <span style={{ color: 'gray' }}> Size: </span> {size}
+                </p>
+
+                <p style={{ fontWeight: 'bold', fontFamily: 'monospace' }}>
+                  JUMIA <span style={{ color: 'red' }}>EXPRESS</span>
+                </p>
+                <p style={{ color: 'gray' }}>
+                  Eligible for free Delivery for orders $80 and above in Accra
+                  (Excluding Large items)
+                </p>
+              </div>
+
+              <div>
+                <h3>{price}</h3>
+                <p>
+                  <span className={styles.preprice}>{preprice}</span>{' '}
+                  <span className={styles.discount}>{discount}</span>
+                </p>
+              </div>
+            </div>
+
+            <div className={styles.btns}>
+              <button
+                className={styles.margin}
+                onClick={() => {
+                  delProduct(id);
+                }}
+              >
+                REMOVE
+              </button>
+              <div style={{ display: 'flex', alignItems: 'space-around' }}>
+                <button
+                  className={styles.margin}
+                  onClick={() => {
+                    setQuantity(quantity - 1);
+                  }}
+                >
+                  -
+                </button>
+                <p>{quantity}</p>
+                <button
+                  className={styles.margin}
+                  onClick={() => {
+                    setQuantity(quantity + 1);
+                  }}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+            <hr style={{ color: 'rgb(226, 224, 222) ' }} />
+          </div>
+        );
+      })}
+
+      <button
+        style={{ margin: '0 auto' }}
+        onClick={() => {
+          setProducts([]);
+        }}
+      >
+        Remove All Products
+      </button>
+    </section>
+  );
 }
